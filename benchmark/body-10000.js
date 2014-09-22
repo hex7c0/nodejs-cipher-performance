f4acbddeb205325460e5a1e4a38e29c90e16b39d
hex7c0 <carniellifrancesco@gmail.com>
2014-09-22 19:41:21 +0200

    from https://github.com/jshttp/etag by https://github.com/dougwilson

---
+++ benchmark/body-10000.js
@@ -0,0 +1,69 @@
+/**
+ * Module dependencies.
+ */
+
+var benchmark = require('benchmark')
+var benchmarks = require('beautify-benchmark')
+var seedrandom = require('seedrandom')
+
+/**
+ * Globals for benchmark.js
+ */
+
+global.buffer = getbuffer(10000)
+global.cipher = require('..')
+global.string = getbuffer(10000).toString()
+
+var suite = new benchmark.Suite
+
+var encoding = [ 'hex', 'binary', 'base64' ];
+var ciphers = require('crypto').getCiphers();
+for (var i = 0, ii = ciphers.length; i < ii; i++) {
+    var j = 2;
+    // for (var j = 0, jj = encoding.length; j < jj; j++) {
+    // suite.add({
+    // name: ' string-' + encoding[j] + '-' + ciphers[i],
+    // minSamples: 50,
+    // fn: 'var val = cipher(string, "' + ciphers[i] + '", "' + encoding[j]
+    // + '")'
+    // });
+    suite.add({
+        name: ' buffer-' + encoding[j] + '-' + ciphers[i],
+        minSamples: 50,
+        fn: 'var val = cipher(buffer, "' + ciphers[i] + '", Buffer("ciao"), "'
+                + encoding[j] + '")'
+    });
+    // }
+}
+
+suite.on('start', function onCycle(event) {
+
+    process.stdout.write('  10000 body\n\n')
+})
+
+suite.on('cycle', function onCycle(event) {
+
+    benchmarks.add(event.target);
+})
+
+suite.on('complete', function onComplete() {
+
+    benchmarks.log();
+    console.log('Fastest is:' + this.filter('fastest').pluck('name') + '\n');
+})
+
+suite.run({
+    async: false
+})
+
+function getbuffer(size) {
+
+    var buffer = new Buffer(size)
+    var rng = seedrandom('body ' + size)
+
+    for (var i = 0; i < buffer.length; i++) {
+        buffer[i] = (rng() * 94 + 32) | 0
+    }
+
+    return buffer
+}
